16894b6a59a4c4663b8f0be6aabfec25b0f8bacc
Luis-Miguel-Cruz-Linares <a3521110400@alumno.uttehuacan.edu.mx>
2023-01-19 21:18:44 -0600

    space 5A v1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { data } from './app.module';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'space-5A';
+  title = 'space-5b';
+  data: data[] = [];
+  constructor (
+    private http:HttpClient
+  ){}
+
+  ngOnInit(){
+    this.http.get('https://api.spacexdata.com/v4/rockets').subscribe((data:any)=> {this.data = data},
+    err => {console.log(err);});
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,9 +9,26 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+ }
+export class data {
+  name: string;
+  description:string;
+  flickr_images:string;
+  constructor(
+    name: string,
+  description:string,
+  flickr_images:string,
+  ){
+    this.name = name;
+    this.description = description;
+    this.flickr_images = flickr_images;
+  }
+}
