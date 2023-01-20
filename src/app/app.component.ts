64283b617371d50c2d0f18b159b1222fc5cdb6a9
Luis-Miguel-Cruz-Linares <a3521110400@alumno.uttehuacan.edu.mx>
2023-01-20 12:27:43 -0600

    api capsules v1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,25 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { data } from './app.module';
+import { Component,OnInit } from '@angular/core';
+import { CompanyService } from './services/company.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'space-5b';
-  data: data[] = [];
-  constructor (
-    private http:HttpClient
-  ){}
 
-  ngOnInit(){
-    this.http.get('https://api.spacexdata.com/v4/rockets').subscribe((data:any)=> {this.data = data},
-    err => {console.log(err);});
-  }
+
+export class AppComponent implements OnInit {
+  title = 'appi2';
+  info: any [] = [];
+
+
+constructor(private CompanyService: CompanyService) {
+  console.log ('El componente se ha inicializado');
 }
+
+ngOnInit(): void {
+  console.log('El componente se ha inicializado');
+  this.CompanyService.getInfo().subscribe((info: any) => this.info = info);
+ }
+}
+
